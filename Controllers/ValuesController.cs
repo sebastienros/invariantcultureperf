f33b82320085d0bce9aa712caa9f01d850b09b61
Sebastien Ros <sebastienros@gmail.com>
2019-06-04 16:03:18 -0700

    Creating project

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InvariantCulture.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        [Route("InvariantCultureIgnoreCase/{count}")]
+        public ActionResult InvariantCultureIgnoreCase(int count)
+        {
+            var data = new Dictionary<string, int>(StringComparer.InvariantCultureIgnoreCase);
+            for (var i = 0; i < count; i++)
+            {
+                data.TryAdd("Id_", i);
+            }
+
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("OrdinalIgnoreCase/{count}")]
+        public ActionResult OrdinalIgnoreCase(int count)
+        {
+            var data = new Dictionary<string, int>(StringComparer.OrdinalIgnoreCase);
+            for (var i = 0; i < count; i++)
+            {
+                data.TryAdd("Id_", i);
+            }
+
+            return Ok();
+        }
+    }
+}
